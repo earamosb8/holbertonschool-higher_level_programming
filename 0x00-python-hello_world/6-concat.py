57bf3e183e9e26b20438ff3430968a362bc7875c
somarae8 <1493@holbertonschool.com>
2020-04-27 20:07:56 +0000

    fix 6

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
-str2 = "School"
-str3 = " "
-print("Welcome to {:s}{:s}{:s}!".format(str1, str3, str2))
+str2 = "Schol"
+str1 = str1+" "+str2
+print("Welcome to {:s}!".format(str1))
