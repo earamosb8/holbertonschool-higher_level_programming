0de134e29753782ddfe6d30eb767e2f0cce402ce
somarae8 <1493@holbertonschool.com>
2020-04-28 18:02:44 +0000

    fix last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ copy = number
 if number < 0:
     number = number * -1
 last = number % 10
-print("Last digit of {} is {} ".format(number, last), end="")
+print("Last digit of {} is {} ".format(copy, last), end="")
 if last > 5:
     print("and is greater than 5".format(copy, last))
 elif last == 0:
