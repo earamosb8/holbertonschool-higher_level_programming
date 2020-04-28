cffc5863897d0fdbb148683c3e0e6e34430d1676
somarae8 <1493@holbertonschool.com>
2020-04-28 23:10:43 +0000

    fix pow

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,20 +1,3 @@
 #!/usr/bin/env python3
 def pow(a, b):
-    if b == 0:
-        return(1)
-    elif b < 0:
-        b = b * -1
-        for y in range(1, b):
-            a = a * a
-            c = 1 / a
-        return(c)
-    elif a < 0:
-        f = 1
-        for d in range(1, b + 1):
-            f = f * a
-        return(f)
-    else:
-        f = 1
-        for x in range(1, b + 1):
-            f = f * a
-        return(f)
+    return a ** b
