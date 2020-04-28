4e27d08610a6569ba8a4bcd5afc89fc1734de496
somarae8 <1493@holbertonschool.com>
2020-04-28 19:41:18 +0000

    fix 5-print_comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python 3
-for x in range(0, 10):
-    for y in range(0, 10):
-        if x != 9 or y != 9:
-            print("{}{}, ".format(x, y), end="")
-        else:
-            print("{}{}".format(x, y))
+#!/usr/bin/python3
+for x in range(0, 100):
+    if x != 99:
+        print("{}, ".format(x), end="")
+    else:
+        print("{}".format(x))
