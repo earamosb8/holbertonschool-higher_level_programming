25781b24f9fdc51c0d3d793044959cc177342023
somarae8 <1493@holbertonschool.com>
2020-04-29 05:00:16 +0000

    fix 6-printcombo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
 for x in range(0, 9):
-    y = x + 1
-    for y in range(0, 10):
+    for y in range(x + 1, 10):
         if x != 8 or y != 9:
             print("{}{},".format(x, y), end=" ")
         else:
             print("{}{}".format(x, y))
+    
+     
