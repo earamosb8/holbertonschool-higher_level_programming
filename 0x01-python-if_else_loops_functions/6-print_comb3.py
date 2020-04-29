a8d485aeef77e25c1d07e0a9def89fab150b5581
somarae8 <1493@holbertonschool.com>
2020-04-29 04:41:36 +0000

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-a = 0
-b = 0
-for x in range(a, 9):
-    for y in range(b, 10):
+
+for x in range(0, 9):
+    y = x + 1
+    for y in range(0, 10):
         if x != 8 or y != 9:
             print("{}{},".format(x, y), end=" ")
         else:
             print("{}{}".format(x, y))
-    a = a + 1
-    b = a + 1
