0c8c91d03461848fd9b77a81b982538813c5d386
somarae8 <1493@holbertonschool.com>
2020-04-30 20:32:18 +0000

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif len(sys.argv) > 1:
         cont = len(sys.argv)
-        print("{} arguments:".format(cont - 1))
+        if cont == 2:
+            print("{} argument:".format(cont - 1))
+        elif cont > 1:
+            print("{} arguments:".format(cont - 1))
         for x in range(1, cont):
             print("{}: {}".format(x, sys.argv[x]))
