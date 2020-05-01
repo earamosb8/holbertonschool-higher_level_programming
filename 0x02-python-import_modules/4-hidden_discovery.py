9de63446581ded45fa6fb7376d9368173f0fb56f
somarae8 <1493@holbertonschool.com>
2020-05-01 02:42:58 +0000

    fix 4-hidden_discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for x in dir(hidden_4):
-        if x[0] != '_':
-            print("{}".format(x))
+    for y in dir(hidden_4):
+        if y[0] != '_':
+            print("{}".format(y))
