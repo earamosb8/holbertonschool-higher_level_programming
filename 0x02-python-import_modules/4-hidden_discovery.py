dfcd26819e6c5d2a45c0e16105a497aedeeadbfe
somarae8 <1493@holbertonschool.com>
2020-05-01 02:33:55 +0000

    fix 4-hidden_discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    com = len(dir(hidden_4))
-    res = dir(hidden_4)
-    for x in range(0, com):
-        f = res[x].find("__")
-        if f == -1:
-            print("{}".format(res[x]))
+    for x in dir(hidden_4):
+        if x[0] != ('_'):
+            print("{}".format(x))
