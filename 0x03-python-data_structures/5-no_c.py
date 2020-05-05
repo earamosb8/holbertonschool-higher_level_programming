07e75054c9cf35c683f7d07fe82bdfd6ab5b18f5
somarae8 <1493@holbertonschool.com>
2020-05-05 19:45:32 +0000

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    if my_string:
-        new_string = ""
-        for l in my_string:
-            if l not in "cC":
-                new_string = new_string + l
-        return(new_string)
+    new_string = ""
+    for l in my_string:
+        if l not in "cC":
+            new_string = new_string + l
+    return(new_string)
