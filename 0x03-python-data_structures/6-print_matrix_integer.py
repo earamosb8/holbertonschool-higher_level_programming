3c4c7985001cc9fe98025474e8ba59a8a234cc22
somarae8 <1493@holbertonschool.com>
2020-05-06 03:37:04 +0000

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for cas in row:
-            print("{:d}".format(cas), end="")
-            if cas != row[-1]:
-                print(" ", end="")
-        print(" ")
+    """ x_row : row of matrix """
+    """ cas : index of row""""
+    for x_row in matrix:
+        for cas in x_row:
+            if cas != x_row[len(x_row) - 1]:
+                print("{:d}".format(cas), end=" ")
+            else:
+                print("{:d}".format(cas), end="")
+        print("")
