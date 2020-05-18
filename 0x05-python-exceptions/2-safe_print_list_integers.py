ce04180cff49a79d5f7f3c9e3140feb3e617d525
somarae8 <1493@holbertonschool.com>
2020-05-18 21:04:14 +0000

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,8 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
             c = c + 1
         except ValueError:
             pass
-        except IndexError:
-            pass
         except TypeError:
             pass
     print("")
