f761e3199821ba59d27f15ee78b844c08b81e8b8
somarae8 <1493@holbertonschool.com>
2020-05-18 17:34:17 +0000

    fix 0-safe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    cont = 0
-    for elemento in my_list:
-        cont += 1
     try:
         for i in range(0, x):
-            print("{}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end='')
         return(i + 1)
+        print("\n")
     except IndexError:
-        return(cont)
+        return(i)
