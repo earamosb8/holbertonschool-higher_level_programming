e40a0de4f3cb6a4a8d38e1f701385594f369f8e6
somarae8 <1493@holbertonschool.com>
2020-05-19 21:00:02 +0000

    fix 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""This module creates a new class Square"""
+
+
 class Square:
+    """ Class Square constuctor method"""
     def __init__(self, size):
+        """Initializes the data."""
         self.__size = size
