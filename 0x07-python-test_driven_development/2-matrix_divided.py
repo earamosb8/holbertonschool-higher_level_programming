9d89fa7ba2b6046a00b69c129cb16ce5d4231518
somarae8 <1493@holbertonschool.com>
2020-05-22 23:10:38 +0000

    2-matrix_divided.py and test

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,21 +3,25 @@
 
 
 def matrix_divided(matrix, div):
-    mark = len(matrix[0])
-    my_list = [[]]
-    i = 0
-    if isinstance(div, str) or i is None:
-        raise TypeError("div must be a number")
+    """this functions divided a matrix, Return New matrix"""
+
+    my_matrix = [i[:] for i in matrix]
+    if matrix is None:
+        raise TypeError("matrix must be a matrix (list of lists)\
+     of integers/floats")
     if div == 0:
-        raise ZeroDivisionError("ZeroDivisionError")
-    for i in range(len(matrix)):
-        for x in matrix[i]:
-            if isinstance(matrix[i][x], str) or i is None:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            else:
-                my_list[i][x].append(matrix[i][x] / div)
-    return my_list 
-                
-   
-    
-    
+        raise ZeroDivisionError('division by zero')
+    if isinstance(div, str) is True:
+        raise TypeError('div must be a number')
+    for x in range(0, len(matrix)):
+        for y in range(0, len(matrix[0])):
+            if isinstance(matrix[x][y], (int, float)) is False:
+                raise TypeError("matrix must be a matrix (list of lists)\
+     of integers/floats")
+    for i in range(0, len(matrix)):
+        if (len(matrix[0]) != len(matrix[i])):
+            raise TypeError("Each row of the matrix must have the same size")
+    for i in range(0, len(my_matrix)):
+            for j in range(0, len(my_matrix[0])):
+                my_matrix[i][j] = round(my_matrix[i][j] / div, 2)
+    return(my_matrix)
