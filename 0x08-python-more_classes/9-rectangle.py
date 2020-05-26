3b8f24f3216d691753cc393032f85e9c85a8772f
somarae8 <1493@holbertonschool.com>
2020-05-26 01:21:35 +0000

    9-rectangle.py fix

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -10,14 +10,13 @@ class Rectangle:
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """check which is the largest object"""
-        if isinstance(rect_1, Rectangle) is false:
+        if type(rect_1) is not Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if isinstance(rect_2, Rectangle) is false:
+        if type(rect_2) is not Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
-            return(rect_1)
-        else:
-            return(rect_2)
+            return (rect_1)
+        return(rect_2)
 
     def __init__(self, width=0, height=0):
         self.height = height
