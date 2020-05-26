410de83c41b7bdd97667356b0a334390774f3eea
somarae8 <1493@holbertonschool.com>
2020-05-26 01:29:22 +0000

    9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -7,17 +7,6 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = "#"
 
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """check which is the largest object"""
-        if type(rect_1) is not Rectangle:
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if type(rect_2) is not Rectangle:
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
-            return (rect_1)
-        return(rect_2)
-
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
@@ -84,7 +73,17 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
+    def bigger_or_equal(rect_1, rect_2):
+        """Compares 2 rectangles"""
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return (rect_1)
+        return(rect_2)
+
     @classmethod
     def square(cls, size=0):
-        """Create a Square"""
+        """Creates a square"""
         return (cls(size, size))
