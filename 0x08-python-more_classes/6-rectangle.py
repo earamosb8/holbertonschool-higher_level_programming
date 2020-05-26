1bba3a7f23b07183dd3ecada5de7f78cf0b27ef0
somarae8 <1493@holbertonschool.com>
2020-05-26 02:01:37 +0000

    fix all task

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle():
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
         Rectangle.number_of_instances += 1
 
     @property
