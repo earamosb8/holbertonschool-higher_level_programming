e5c40934f1a3a7a0b4210127807a4f1ee3d8e00f
somarae8 <1493@holbertonschool.com>
2020-06-03 23:39:14 +0000

    5-to_json_string.py

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+"""This is module 5-to_json_string."""
+
+
+def to_json_string(my_obj):
+    """transforms an object in a json representation"""
+    return json.dumps(my_obj)
