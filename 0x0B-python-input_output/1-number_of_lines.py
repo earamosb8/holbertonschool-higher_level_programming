5cbe7d9a2de4b6c2badad70950f41372b292d5ba
somarae8 <1493@holbertonschool.com>
2020-06-03 22:59:02 +0000

    1-number_of_lines.py fix

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -6,7 +6,7 @@ def number_of_lines(filename=""):
     """print the lines of a file"""
 
     lines = 0
-    with open('my_file_0.txt', 'r', encoding="utf-8") as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         for line in f:
             lines += 1
     return lines
