32db8192d4c41c8e2faa59ffa93abf90a2f3200a
somarae8 <1493@holbertonschool.com>
2020-06-03 22:56:54 +0000

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 def read_file(filename=""):
     """reads and prints a file"""
 
-    with open('my_file_0.txt', 'r', encoding="utf-8") as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
     print(read_data, end="")
