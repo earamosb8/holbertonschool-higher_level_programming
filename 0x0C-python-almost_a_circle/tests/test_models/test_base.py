13cccdcb03950f63b682fc31378daa9feedb645b
somarae8 <1493@holbertonschool.com>
2020-06-09 21:24:22 +0000

    fix test _base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,7 +3,11 @@
 """Unittest for Base class"""
 
 import unittest
+import os
+import json
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
@@ -19,10 +23,12 @@ class TestBase(unittest.TestCase):
         b1 = Base()
         b2 = Base(12)
         b3 = Base()
+        b4 = Base(-1)
         self.assertEqual(b0.id, 1)
         self.assertEqual(b1.id, 2)
         self.assertEqual(b2.id, 12)
         self.assertEqual(b3.id, 3)
+        self.assertEqual(b4.id, -1)
 
     def test_1(self):
         """Test number 2 for base"""
