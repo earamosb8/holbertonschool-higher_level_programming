60d5ebc2806b64b377156cda17514b2b2abfb1f2
somarae8 <1493@holbertonschool.com>
2020-06-09 21:07:57 +0000

    add test

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+"""Unittest for Base class"""
+
+import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    """Base tests"""
+
+    def test_0(self):
+        """Test number 0 for base"""
+        b0 = Base()
+        b1 = Base()
+        b2 = Base(12)
+        b3 = Base()
+        self.assertEqual(b0.id, 1)
+        self.assertEqual(b1.id, 2)
+        self.assertEqual(b2.id, 12)
+        self.assertEqual(b3.id, 3)
+
+    def test_1(self):
+        """Test number 2 for base"""
+        b0 = Base(id=None)
+        self.assertEqual(b0.id, 1)
+
+    def test_2(self):
+        """Test number 1 for base"""
+        b0 = Base("h")
+        self.assertEqual(b0.id, "h")
+
+    def test_3(self):
+        """Test number 3 for base"""
+        b0 = Base(0.03)
+        self.assertEqual(b0.id, 0.03)
+
+    def test_4(self):
+        """Test number 4 for base"""
+        b0 = Base(False)
+        self.assertEqual(b0.id, False)
+
+    def test_5(self):
+        """Test number 5 for base"""
+        b0 = Base(-16)
+        self.assertEqual(b0.id, -16)
+
+if __name__ == '__main__':
+    unittest.main()
