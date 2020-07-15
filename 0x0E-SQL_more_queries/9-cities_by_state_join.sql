bb349b28bab3f6d3c398ecab87026d6569a4642b
somarae8 <1493@holbertonschool.com>
2020-07-15 02:51:02 +0000

    add 8,9 tasks

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,4 @@
+-- script that lists all cities contained in the database hbtn_0d_usa.
+-- Each record should display: cities.id - cities.name - states.name
 SELECT cities.id, cities.name, states.name FROM cities
 LEFT JOIN states ON cities.state_id = states.id;
