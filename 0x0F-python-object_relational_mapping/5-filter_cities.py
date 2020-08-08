5425fdbd5e4cadfaeedb0c574f8fa5fc5701f016
somarae8 <somarae8@gmail.com>
2020-08-08 15:39:20 +0000

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
-
+"""This module defines a query"""
 
 import MySQLdb
 from sys import argv
 
-
-def matches():
+if __name__ == "__main__":
     """takes a state as an argument and lists all cities of that state"""
     username = argv[1]
     password = argv[2]
     dbname = argv[3]
     states = argv[4]
     my_list = []
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         password=password, db=dbname)
-    cur = db.cursor()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           password=password, db=dbname)
+    cur = conn.cursor()
     cur.execute("SELECT cities.name\
                  from cities JOIN states ON states.id = cities.state_id\
-                 WHERE states.name = %s", [states])
+                 WHERE states.name = %s ORDER BY cities.id", [states])
     rows = cur.fetchall()
     for x in rows:
         for y in x:
             my_list.append(y)
     print(", ".join(my_list))
-
-if __name__ == "__main__":
-    matches()
+    cur.close()
+    conn.close()
