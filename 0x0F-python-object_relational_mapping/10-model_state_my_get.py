ea64e78cab8836b3f3e4dbc72a7d68000e070bbc
somarae8 <somarae8@gmail.com>
2020-08-08 18:44:40 +0000

    10-model_state_my_get.py

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""prints the State object with the name passed as argument"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    namestate = sys.argv[4]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(engine)()
+    states = session.query(State).filter(namestate == State.name).first()
+    if states:
+        print("{}".format(states.id))
+    else:
+        print("Not Found")
+    session.close()
