0890c6afaa12c04655736d1858dd5bb930f6e2c7
somarae8 <somarae8@gmail.com>
2020-08-08 14:46:50 +0000

    0-select_states.py udp

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ def select():
     username = argv[1]
     password = argv[2]
     dbname = argv[3]
-    db = MySQLdb.connect(host="127.0.0.1", port=3306,
+    conn = MySQLdb.connect(host="127.0.0.1", port=3306,
                          user=username, passwd=password, db=dbname)
 
     cursor = db.cursor()
@@ -18,6 +18,9 @@ def select():
     rows = cursor.fetchall()
     for x in rows:
         print(x)
+    cursor.close()
+    conn.close()
+    
 
 if __name__ == "__main__":
     select()
