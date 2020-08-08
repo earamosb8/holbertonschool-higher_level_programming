5311f1837981e3bddd26bd20818960f94de9465e
somarae8 <somarae8@gmail.com>
2020-08-08 15:20:54 +0000

    2-my_filter_states.py fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-
+"""This module defines a query"""
 
 import MySQLdb
 from sys import argv
 
 
-def matches():
+if __name__ == "__main__":
     """displays all values in the table where name matches the argument"""
     username = argv[1]
     password = argv[2]
     dbname = argv[3]
     search = argv[4]
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         password=password, db=dbname)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{}'".format(search))
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           password=password, db=dbname)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name='{}'\
+                ORDER BY id ASC".format(search))
     rows = cur.fetchall()
     for x in rows:
         print(x)
-
-if __name__ == "__main__":
-    matches()
+    cur.close()
+    conn.close()
