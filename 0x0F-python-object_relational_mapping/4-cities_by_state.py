3e23b74319c488b7ac554d59c0aa3e98a84de0da
somarae8 <somarae8@gmail.com>
2020-08-08 15:35:33 +0000

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-
+"""This module defines a query"""
 
 import MySQLdb
 from sys import argv
 
 
-def mainjoin():
+if __name__ == "__main__":
     """lists all cities from the database hbtn_0e_4_usa."""
     username = argv[1]
     password = argv[2]
     database = argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=database)
-    cur = db.cursor()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=database)
+    cur = conn.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name from cities\
                  JOIN states ON states.id = cities.state_id ORDER BY id ASC")
     rows = cur.fetchall()
     for x in rows:
         print(x)
-if __name__ == "__main__":
-    mainjoin()
+    cur.close()
+    conn.close()
