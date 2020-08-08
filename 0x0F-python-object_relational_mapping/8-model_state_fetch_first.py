e4f93e420f56a6b6f1a555dbd227220584ed4ea0
somarae8 <somarae8@gmail.com>
2020-08-08 18:11:36 +0000

    8-model_state_fetch_first.py

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module defines a query"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(engine)()
+    state = session.query(State).order_by(State.id).first()
+    print("{}: {}".format(state.id, state.name))
+    session.close()
