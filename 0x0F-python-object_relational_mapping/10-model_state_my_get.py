61449f636f8736f70cff2509578b532751597fdf
somarae8 <somarae8@gmail.com>
2020-08-09 03:18:41 +0000

    10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-"""prints the State object with the name passed as argument"""
+""" print all State from the database"""
+
 import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-
 
 if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    statename = sys.argv[4]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(username, password, database),
-                           pool_pre_ping=True)
-    session = sessionmaker(bind=engine)()
-    sts = session.query(State).filter(State.name == statename).first()
-    if sts:
-        print(sts.id)
-    else:
-        print('Not Found')
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+    STATE = sys.argv[4]
 
-    session.close()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    s = sessionmaker(bind=engine)()
+
+    _query = s.query(State).filter(State.name == STATE).first()
+    if _query:
+        print(_query.id)
+    else:
+        print('Not found')
+
+    s.close()
