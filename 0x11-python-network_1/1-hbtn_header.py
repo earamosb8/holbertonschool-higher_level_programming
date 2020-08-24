4fdf9db5720dcea7851d07c89a5e7140b56f1010
somarae8 <somarae8@gmail.com>
2020-08-24 21:13:59 +0000

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""script that fetches https://intranet.hbtn.io/status"""
+"""takes in a URL, sends a request to the URL 
+   and displays the value of the X-Request-Id
+   variable found in the header of the response"""
 
 
 import sys
 import urllib.request
 
-with urllib.request.urlopen(sys.argv[1]) as html:
-    print(html.headers['X-Request-Id'])
+if __name__ == '__main__':
+    with urllib.request.urlopen(sys.argv[1]) as html:
+        print(html.headers['X-Request-Id'])
