05c9e2fde7d0ca9706c1dba01921fd285ed7db03
somarae8 <somarae8@gmail.com>
2020-08-24 20:49:37 +0000

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-# Returns a specific URL and displays some info about the response.
 import urllib.request
+
+"""script that fetches https://intranet.hbtn.io/status"""
+
+
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-   html = response.read()
-   print("Body response:")
-   print("\t- type: {}".format(type(html)))
-   print("\t- content: {}".format(html))
-   print("\t- utf8 content: {}".format(html.decode("utf-8")))
+    html = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode("utf-8")))
