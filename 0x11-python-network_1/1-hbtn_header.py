2da8a1771bbda57e5b0e71f3e3da4b8d884cac80
somarae8 <somarae8@gmail.com>
2020-08-24 21:08:12 +0000

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import sys
-import urllib.request
-
 """script that fetches https://intranet.hbtn.io/status"""
 
+
+import sys
+import urllib.request
 
 with urllib.request.urlopen(sys.argv[1]) as html:
     print(html.headers['X-Request-Id'])
