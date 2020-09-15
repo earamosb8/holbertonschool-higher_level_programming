3378d2f24aefb6de03db72afd60a8a83b31dae2d
somarae8 <somarae8@gmail.com>
2020-09-15 21:44:35 +0000

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// script that prints a square
+if (process.argv.length === 2) {
+  console.log('Missing size');
+} else if (process.argv.length === 3) {
+  const num = parseInt(process.argv[2]);
+  if (Number.isInteger(num)) {
+    for (let i = 0; i < process.argv[2]; i++) {
+      console.log('X'.repeat(num));
+    }
+  }
+}
