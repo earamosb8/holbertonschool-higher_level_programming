a3089f6e6a74683b16b7921b295380321b117a98
somarae8 <somarae8@gmail.com>
2020-09-15 18:07:02 +0000

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // script that prints a message depending of the number of arguments passed
 if (process.argv[2]) {
-  console.log('Argument found');
+  console.log(process.argv[2]);
 } else {
   console.log('No argument');
 }
