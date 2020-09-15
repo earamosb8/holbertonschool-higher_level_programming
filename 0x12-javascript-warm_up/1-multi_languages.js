0b665c6fd1def3f802ea425134a2c44c38cbda72
somarae8 <somarae8@gmail.com>
2020-09-15 17:43:23 +0000

    1-multi_languages.js updt

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 //script that prints 3 lines
-const first_line = "C is fun";
-const second_line = "Python is cool";
-const third_line = "Javascript is amazing";
-console.log(first_line + "\n" + second_line + "\n" + third_line);
+const first_line = 'C is fun';
+const second_line = 'Python is cool';
+const third_line = 'Javascript is amazing';
+console.log(first_line + '\n' + second_line + '\n' + third_line);
