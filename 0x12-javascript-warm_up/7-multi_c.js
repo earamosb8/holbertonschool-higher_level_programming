303a068a1251b29ec5fd30d5ede7e519e652b769
somarae8 <somarae8@gmail.com>
2020-09-15 21:18:59 +0000

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 // script that prints x times C is fun
-for (let i = 0; i < process.argv[2]; i++) {
-  console.log( 'C is fun');
+if (process.argv.length === 2) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log('C is fun');
+  }
 }
