cd79b5302e57599ad626f4e5666502bf9bfc5f09
somarae8 <somarae8@gmail.com>
2020-09-21 17:34:22 -0500

    add condicional in class rectangle

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Rectangle class with constructor(width and height)
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
