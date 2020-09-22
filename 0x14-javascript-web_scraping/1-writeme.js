d7ec0ecd910acfd2f10dbec2037b614beb0adbc0
somarae8 <somarae8@gmail.com>
2020-09-22 16:02:44 -0500

    fix task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 // write a file and print the content
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], function (err, data) {
+fs.writeFile(process.argv[2], process.argv[3], function (err) {
   if (err) {
     return console.log(err);
   }
-  console.log(data);
 });
