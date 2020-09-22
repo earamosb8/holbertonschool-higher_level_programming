3f30634df90d55f453722d66432309d6babb24c0
somarae8 <somarae8@gmail.com>
2020-09-21 20:51:12 -0500

    update

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,7 @@ const SquareOne = require('./5-Square');
 
 module.exports = class Square extends SquareOne {
   charPrint (c = 'X') {
-    for (let i = 0; i < this.height; i++) {
+    for (let i = 1; i <= this.height; i++) {
       console.log(c.repeat(this.width));
     }
   }
