547963b3bff2d1aa0e2b48d0c7ab464a3cd0076e
somarae8 <somarae8@gmail.com>
2020-09-21 20:27:18 -0500

    add new class square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// other Square class that inhererit from Square
+const SquareOne = require('./5-Square');
+
+module.exports = class Square extends SquareOne {
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
