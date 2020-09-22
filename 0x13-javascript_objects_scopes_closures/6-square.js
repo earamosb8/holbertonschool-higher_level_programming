f047603fd064551e3ff927d9a212e67309812b1c
somarae8 <somarae8@gmail.com>
2020-09-21 21:46:51 -0500

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// other Square class that inherit from Square:::
+
+const squareOne = require('./5-Square');
+
+module.exports = class Square extends squareOne {
+  charPrint (c = 'X') {
+    for (let i = 1; i <= this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
+
